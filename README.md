ea72c30d1d59cb54557857df46bd7e3dcfaa0a50
Sachit Karki <sachitjungkarki@gmail.com>
2021-03-20 19:47:47 +0545

    more checks and improvements

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # active-wifi-devices
 
-To list all my wifi connected devices on all of my routers.
+To list all my wifi connected devices on all of my routers. My main router is RaiseComm(Vianet) `Device Model: HT803-N` and Secondary router is Trade-Link `Device Model: T31D`
 
 This is just a personal project. I only made this so I dont' have to keep track of all the mac address connected to my router and see which device is which.
 
@@ -11,9 +11,12 @@ Run this line to copy and add the mac address and credentials for routers
 cp env_example.py env.py
 ```
 
-macDeviceDict format is
+macDeviceDict format in `env.py` is
 ```
 {
   "ab-12-cd-34-ef-56": "One plus 9 Pro"
 }
 ```
+
+### Preview
+![Preview](https://raw.githubusercontent.com/tsachit/active-wifi-devices/main/ScreenShot.png)
--- index.py
+++ index.py
@@ -11,19 +11,22 @@ def sanitizeMac(mac):
 
 # Vianet RaiseCom Router
 def loginMain():
-    url = "http://192.168.1.1/boaform/admin/formLogin"
-    payload = f"username={mainUsername}&psd={mainPassword}&loginSelinit=1"
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Cookie': cookie
-    }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    if response.status_code == 200:
-        return True
-    return False
+    try:
+        url = "http://192.168.1.1/boaform/admin/formLogin"
+        payload = f"username={mainUsername}&psd={mainPassword}&loginSelinit=1"
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Cookie': cookie
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        if response.status_code == 200:
+            return True
+        return False
+    except:
+        return False
 
 
-def getTableFromMainRouter(retries=0):
+def getTableFromMainRouter(retries=1):
     try:
         url = "http://192.168.1.1/status_wlan_info_11n.asp"
         payload = {}
@@ -36,26 +39,29 @@ def getTableFromMainRouter(retries=0):
         table = soup.findAll('table', attrs={'class': 'flat'})[2]
         return pd.read_html(table.prettify())[0]
     except:
-        if(retries <= 2):
-            print(retries)
+        if(retries <= 3):
+            print(f"Retrying login on main router count: {retries}")
             retries += 1
             loginMain()
             return getTableFromMainRouter(retries)
 
 # Tradelink router
 def loginSecondary():
-    url = "http://192.168.100.1/login.cgi"
-    payload = f"username={secondaryUsername}&password={secondaryPassword}&submit.htm%3Flogin.htm=Send"
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Cookie': cookie
-    }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    if response.status_code == 200:
-        return True
-    return False
+    try:
+        url = "http://192.168.100.1/login.cgi"
+        payload = f"username={secondaryUsername}&password={secondaryPassword}&submit.htm%3Flogin.htm=Send"
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Cookie': cookie
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        if response.status_code == 200:
+            return True
+        return False
+    except:
+        return False
 
-def getTableFromSecondaryRouter(retries=0):
+def getTableFromSecondaryRouter(retries=1):
     try:
         url = "http://192.168.100.1/dhcptbl.htm"
         payload = {}
@@ -69,11 +75,11 @@ def getTableFromSecondaryRouter(retries=0):
             'table', attrs={'class': 'formlisting'})[0]
         return pd.read_html(table.prettify())[0]
     except:
-        if(retries <= 2):
-            print(retries)
+        if(retries <= 3):
+            print(f"Retrying login on secondary router count: {retries}")
             retries += 1
             loginSecondary()
-            return getTableFromMainRouter(retries)
+            return getTableFromSecondaryRouter(retries)
 
 
 macAddresses = {}
@@ -81,29 +87,33 @@ for key in macDeviceDict:
     macAddresses[sanitizeMac(key)] = macDeviceDict[key]
 
 tableMain = getTableFromMainRouter()
-if not (tableMain is None or tableMain.empty):
-    macListMain = [['Mac', 'Device Name']]
-    for index, row in tableMain[0].iteritems():
-        if row != 'MAC':
-            cleanMac = sanitizeMac(row)
-            deviceName = macAddresses[cleanMac] if cleanMac in macAddresses else 'Unknown Device'
-            macListMain.append([row, deviceName])
-    mainList = pd.DataFrame(macListMain)
+if not (tableMain is None or tableMain.empty) and 0 in tableMain:
+    macColumn = tableMain[0]
+    if 0 in macColumn and macColumn[0] == 'MAC':
+        macListMain = [['Mac', 'Device Name']]
+        for index, row in macColumn.iteritems():
+            if row != 'MAC':
+                cleanMac = sanitizeMac(row)
+                deviceName = macAddresses[cleanMac] if cleanMac in macAddresses else 'Unknown Device'
+                macListMain.append([row, deviceName])
+        mainList = pd.DataFrame(macListMain)
 
-    print('Connected to main router')
-    print(mainList)
+        print('Connected to main router')
+        print(mainList.to_string(index=False))
 
 
 tableSecondary = getTableFromSecondaryRouter()
-if not (tableSecondary is None or tableSecondary.empty):
-    macListSecondary = [['Mac', 'Device Name']]
-    for index, row in tableSecondary[2].iteritems():
-        if row != 'MAC Address':
-            cleanMac = sanitizeMac(row)
-            deviceName = macAddresses[cleanMac] if cleanMac in macAddresses else 'Unknown Device'
-            macListSecondary.append([row, deviceName])
-    scondaryList = pd.DataFrame(macListSecondary)
+if not (tableSecondary is None or tableSecondary.empty) and 2 in tableSecondary:
+    macColumn = tableSecondary[2]
+    if 0 in macColumn and macColumn[0] == 'MAC Address':
+        macListSecondary = [['Mac', 'Device Name']]
+        for index, row in macColumn.iteritems():
+            if row != 'MAC Address':
+                cleanMac = sanitizeMac(row)
+                deviceName = macAddresses[cleanMac] if cleanMac in macAddresses else 'Unknown Device'
+                macListSecondary.append([row, deviceName])
+        scondaryList = pd.DataFrame(macListSecondary)
 
-    print('\n')
-    print('Connected to secondary router')
-    print(scondaryList)
+        print('\n')
+        print('Connected to secondary router')
+        print(scondaryList.to_string(index=False))
